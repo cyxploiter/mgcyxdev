40cde089527c8238de4f2b0798b2fd6798b2e8d3
cyxploiter <cyxploiter@protonmail.com>
2022-06-11 23:32:33 +0530

    idfkdfkdj

--- Events/Member/promo.js
+++ Events/Member/promo.js
@@ -5,7 +5,7 @@ client.on("guildMemberAdd", async (member) => {
 
     if (member.guild.id === "885977110911545424") return;
 
-    member.send({
+    await member.send({
         content: `https://discord.gg/YCjR7REv4w`,
         components: [new Discord.MessageActionRow().addComponents(new Discord.MessageButton().setURL("https://discord.gg/YCjR7REv4w").setLabel("Pls consider joining box").setStyle("LINK"), new Discord.MessageButton().setLabel(`Sent from ${message.guild.name}`).setStyle("SECONDARY").setCustomId("sentFrom").setDisabled(true))]
     }).catch(err => {
